b8c836ff1ae8725d1720f390caf4aa241f5825e7
Stanislav Plakhin <plakhin@gmail.com>
2016-11-01 16:45:24 +0600

    adds text to twitter share

--- src/social-sharing.js
+++ src/social-sharing.js
@@ -72,7 +72,7 @@ export default {
         },
 
         twitter: {
-          sharer: 'https://twitter.com/intent/tweet?url=@url&via=@twitteruser'
+          sharer: 'https://twitter.com/intent/tweet?text=@title&url=@url&via=@twitteruser'
         },
 
         googleplus: {
