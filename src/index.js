b333f7bff2648ca2c7204db606d91b1c621b5a81
Nicolas Beauvais <nicolas@bvs.email>
2017-12-11 19:19:51 +0100

    Tag 2.3.3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import SocialSharing from './social-sharing';
 
-SocialSharing.version = '2.3.1';
+SocialSharing.version = '2.3.3';
 
 SocialSharing.install = (Vue) => {
   Vue.component('social-sharing', SocialSharing);
